ca1fca2bb6fdbb15b839dc7a2d7e40da6b251e7a
Pratiksha0401 <pratiksha411998@gmail.com>
2025-07-23 00:09:19 +0530

    Longest Subarray with given Sum K positives Better appoarach

--- src/main/java/org/example/array/easy/LongestSubArraySumKPositive.java
+++ src/main/java/org/example/array/easy/LongestSubArraySumKPositive.java
@@ -7,6 +7,14 @@ public class LongestSubArraySumKPositive {
         int n = arr.length;
         int longestSubArrayLength = getLongestSubArrayLengthBruteForce(arr, k);
         System.out.println(longestSubArrayLength);
+
+
+
+        int arr1[] = {1,2,3,1,4,2,1,1,2,1,3};
+        int k1 = 7;
+        int n1 = arr1.length;
+        int longestSubArrayLength1 = getLongestSubArrayLengthBetter(arr1, k);
+        System.out.println(longestSubArrayLength1);
     }
 
     public static int getLongestSubArrayLengthBruteForce(int [] arr,int k){
@@ -19,6 +27,20 @@ public class LongestSubArraySumKPositive {
                 }
                 if(sum==k){
                     mLen = Math.max(mLen,j-i);
+                }
+            }
+        }
+        return mLen;
+    }
+
+    public static int getLongestSubArrayLengthBetter(int [] arr,int k){
+        int mLen = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int sum =0;
+            for (int j = i; j < arr.length; j++) {
+                sum+=arr[j];
+                if(sum==k){
+                    mLen = Math.max(mLen,j-i+1);
                 }
             }
         }
