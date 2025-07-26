c02de64e42d98cdd55bedc3cafd244d851b95360
Pratiksha0401 <pratiksha411998@gmail.com>
2025-07-26 13:54:42 +0530

    Refactored

--- src/main/java/org/example/array/hard/ThreeSumFindTripletsThatAddUpTo0.java
+++ src/main/java/org/example/array/hard/ThreeSumFindTripletsThatAddUpTo0.java
@@ -6,6 +6,7 @@ public class ThreeSumFindTripletsThatAddUpTo0 {
 
     public static void main(String[] args) {
         int arr[] = {-1,-2,-3,-1,0,-1,1,2,-1,0,0,2,3,1,-1,-2};
+        int target = 0;
 
         final List<List<Integer>> lists = bruteForceApproach(arr);
         System.out.println("Brute Force: " + lists);
