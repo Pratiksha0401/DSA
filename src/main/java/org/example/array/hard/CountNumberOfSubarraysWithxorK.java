680ce0ecfcdd6d3f521a32aff868553c27731cc9
Pratiksha0401 <pratiksha411998@gmail.com>
2025-08-05 07:12:33 +0530

    Count the number of subarrays with given xor k brute force

--- src/main/java/org/example/array/hard/CountNumberOfSubarraysWithxorK.java
+++ src/main/java/org/example/array/hard/CountNumberOfSubarraysWithxorK.java
@@ -6,11 +6,34 @@ public class CountNumberOfSubarraysWithxorK {
     public static void main(String[] args) {
         int[] arr={4,2,2,6,4};
         int target =6;
-        int resultCnt = xorOptimal(arr, target);
-        System.out.println(resultCnt);
+
+        int resultCnt1 = bruteForce(arr, target);
+        System.out.println(resultCnt1);
+
+        int resultCnt3 = xorOptimal(arr, target);
+        System.out.println(resultCnt3);
 
     }
 
+
+    //bruteForce
+    public static int bruteForce(int[] nums, int k){
+        int count =0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                int xor =0;
+                for (int l = i; l <= j; l++) {
+                    xor = xor ^ nums[l];
+                }
+                if(xor==k){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    //optimal with xor
     public static int xorOptimal(int[] nums, int k){
         int count =0;
         HashMap<Integer,Integer> frontxor = new HashMap<>();
