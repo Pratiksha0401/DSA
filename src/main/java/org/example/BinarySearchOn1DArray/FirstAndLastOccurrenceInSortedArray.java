3035728eb31ff0892aec35fe03b17d630e1b2ca2
Pratiksha0401 <pratiksha411998@gmail.com>
2025-08-15 07:59:19 +0530

    First and Last occurrence in a sorted array : Added Documentation

--- src/main/java/org/example/BinarySearchOn1DArray/FirstAndLastOccurrenceInSortedArray.java
+++ src/main/java/org/example/BinarySearchOn1DArray/FirstAndLastOccurrenceInSortedArray.java
@@ -10,6 +10,12 @@ public class FirstAndLastOccurrenceInSortedArray {
         // returning the first and last occurrence index if the element is present otherwise -1
         System.out.println("First occurrence of " + key + " = " + first(n, key, v));
         System.out.println("Last occurrence of " + key + " = " + last(n, key, v));
+
+        final int solve = lastOptimal(n, key, v);
+        System.out.println("Last occurrence (optimal) of " + key + " = " + solve);
+
+        final int i = firstOptimal(n, key, v);
+        System.out.println("First occurrence (optimal) of " + key + " = " + i);
     }
 
     /**
@@ -55,6 +61,70 @@ public class FirstAndLastOccurrenceInSortedArray {
             if (v[i] == key) {
                 res = i;
                 break;
+            }
+        }
+        return res;
+    }
+
+    /**
+     * Finds the last occurrence index of the given key in a sorted array using binary search.
+     *
+     * @param n   the size of the array
+     * @param key the target element to search
+     * @param v   the sorted input array
+     * @return index of the last occurrence of key, or -1 if not found
+     *
+     * Time Complexity: O(log n)
+     *   - Each iteration halves the search space.
+     * Space Complexity: O(1)
+     *   - Only a constant amount of extra space is used.
+     */
+    public static int lastOptimal(int n, int key, int[] v) {
+        int start = 0;
+        int end = n - 1;
+        int res = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (v[mid] == key) {
+                res = mid;
+                start = mid + 1;  // move right to find later occurrence
+            } else if (key < v[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return res;
+    }
+
+    /**
+     * Finds the first occurrence index of the given key in a sorted array using binary search.
+     *
+     * @param n   the size of the array
+     * @param key the target element to search
+     * @param v   the sorted input array
+     * @return index of the first occurrence of key, or -1 if not found
+     *
+     * Time Complexity: O(log n)
+     *   - Each iteration halves the search space.
+     * Space Complexity: O(1)
+     *   - Only a constant amount of extra space is used.
+     */
+    public static int firstOptimal(int n, int key, int[] v) {
+        int start = 0;
+        int end = n - 1;
+        int res = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (v[mid] == key) {
+                res = mid;
+                end = mid - 1;  // move left to find earlier occurrence
+            } else if (key < v[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
         }
         return res;
