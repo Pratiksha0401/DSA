4106685eac281356ad33145d1abd870bf7e24751
Pratiksha0401 <pratiksha411998@gmail.com>
2025-08-16 20:42:05 +0530

    Count Occurrences in Sorted: Added Documentation

--- src/main/java/org/example/BinarySearchOn1DArray/CountOccurrencesInSortedArray.java
+++ src/main/java/org/example/BinarySearchOn1DArray/CountOccurrencesInSortedArray.java
@@ -111,7 +111,8 @@ public class CountOccurrencesInSortedArray {
      */
     public static int[] firstAndLastPosition(int[] arr, int n, int k) {
         int first = firstOccurrence(arr, n, k);
-        if (first == -1) return new int[] { -1, -1 };
+        if (first == -1)
+            return new int[] { -1, -1 };
         int last = lastOccurrence(arr, n, k);
         return new int[] {first, last};
     }
@@ -131,7 +132,8 @@ public class CountOccurrencesInSortedArray {
      */
     public static int count(int arr[], int n, int x) {
         int[] ans = firstAndLastPosition(arr, n, x);
-        if (ans[0] == -1) return 0;
+        if (ans[0] == -1)
+            return 0;
         return (ans[1] - ans[0] + 1);
     }
 
