938036a5756157c280ddb9c3342d32a5603b5aee
Pratiksha0401 <pratiksha411998@gmail.com>
2025-08-17 11:24:03 +0530

    Added documentation

--- src/main/java/org/example/hashmap/NoOfDistinctEle.java
+++ src/main/java/org/example/hashmap/NoOfDistinctEle.java
@@ -2,17 +2,39 @@ package org.example.hashmap;
 
 import java.util.HashSet;
 
+/**
+ * Program to find the number of distinct elements in an array.
+ *
+ * <p>Approach:
+ * Use a HashSet because it stores only unique elements.
+ * Simply add all elements from the array into the HashSet
+ * and return its size.</p>
+ *
+ * Example:
+ * arr = [2, 3, 4, 5, 2, 3, 6]
+ * Distinct elements = {2, 3, 4, 5, 6}
+ * Answer = 5
+ *
+ * Time Complexity: O(n)   (each insertion in HashSet is O(1) on average)
+ * Space Complexity: O(n)  (HashSet stores all unique elements)
+ */
 public class NoOfDistinctEle {
     public static void main(String[] args) {
-        int [] arr={2,3,4,5,2,3,6};
+        int[] arr = {2, 3, 4, 5, 2, 3, 6};
         int size = noOfDistinctEle(arr);
-        System.out.println("No of n=distinct element = "+size);
+        System.out.println("Number of distinct elements = " + size);
     }
 
+    /**
+     * Finds the count of distinct elements in an array.
+     *
+     * @param arr input array
+     * @return number of distinct elements
+     */
     private static int noOfDistinctEle(int[] arr) {
         HashSet<Integer> hs = new HashSet<>();
-        for (int i=0;i<arr.length;i++){
-            hs.add(arr[i]);
+        for (int num : arr) {
+            hs.add(num);
         }
         return hs.size();
     }
