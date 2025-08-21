62898f5a68329b4e04129cdab01bd94fbe0b25bf
Pratiksha0401 <pratiksha411998@gmail.com>
2025-08-21 22:32:34 +0530

    Find the Smallest Divisor Given a Threshold : Optimal Approach

--- src/main/java/org/example/BinarySearchAnswers/FindSmallestDivisorGivenThreshold.java
+++ src/main/java/org/example/BinarySearchAnswers/FindSmallestDivisorGivenThreshold.java
@@ -7,6 +7,9 @@ public class FindSmallestDivisorGivenThreshold {
         int limit = 8;
         int ans = smallestDivisorBruteForce(arr, limit);
         System.out.println("The minimum divisor is: " + ans);
+
+        int res = smallestDivisorOptimal(arr, limit);
+        System.out.println("The minimum divisor is: " + res);
     }
 
     public static int smallestDivisorBruteForce(int[] arr, int limit) {
@@ -28,5 +31,37 @@ public class FindSmallestDivisorGivenThreshold {
                 return d;
         }
         return -1;
+    }
+
+    public static int sumByD(int[] arr, int div) {
+        int n = arr.length; //size of array
+        //Find the summation of division values:
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += Math.ceil((double)(arr[i]) / (double)(div));
+        }
+        return sum;
+    }
+    public static int smallestDivisorOptimal(int[] arr, int limit) {
+        int n = arr.length; //size of array.
+        if(n > limit) return -1;
+        //Find the maximum element:
+        int maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, arr[i]);
+        }
+
+        int low = 1, high = maxi;
+
+        //Apply binary search:
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (sumByD(arr, mid) <= limit) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
     }
 }
